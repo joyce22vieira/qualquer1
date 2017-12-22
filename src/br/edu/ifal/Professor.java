ad5335061e11af683545f65f27a97b32dc77464a
Rebeca Alexandre <rebecaalexandre6@gmail.com>
2017-12-22 14:19:26 -0300

    Criacao da classe professor e aluno

---
+++ src/br/edu/ifal/Professor.java
@@ -0,0 +1,35 @@
+package br.edu.ifal;
+
+public class Professor {
+	private String nome;
+	private String formacao;
+	private String cpf;
+	
+	
+	
+	public Professor() {
+		super();
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFormacao() {
+		return formacao;
+	}
+	public void setFormacao(String formacao) {
+		this.formacao = formacao;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
+
